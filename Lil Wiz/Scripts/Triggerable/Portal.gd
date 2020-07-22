b050e2c2399bb0ed448ab8962fde8282aa4aa5cd
Felipe Cardoso <felipecrdos@gmail.com>
2020-07-22 01:13:40 -0300

    delete all

--- Lil Wiz/Scripts/Triggerable/Portal.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-export (String, FILE) var scene
-signal entered_portal(body, scene)
-
-func on_area_body_entered(body):
-	emit_signal("entered_portal", body, scene)
-	$Area/Shape.set_deferred("disabled", true)
