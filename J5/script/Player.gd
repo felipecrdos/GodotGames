1b54638e8b610f9448ce71e0110a127b30cec978
Felipe Cardoso <felipecrdos@gmail.com>
2020-07-22 17:05:57 -0300

    update player

--- J5/script/Player.gd
+++ J5/script/Player.gd
@@ -11,6 +11,10 @@ var state
 var velocity
 var direction
 var speed
+var jump_force
+var can_jump
+var is_jump
+var in_air
 
 func _ready():
 	funcs_names = [	"idle_state", 
@@ -29,9 +33,14 @@ func _ready():
 	state = State.RUNNING
 	set_funcs_refs(funcs_names)
 	
-	velocity = Vector2(100, 100)
-	direction = Vector2(0, 1) 
-	speed = Vector2(100, 9.8)
+	velocity 	= Vector2(100, 100)
+	direction 	= Vector2(0, 1) 
+	speed 		= Vector2(100, 19.6)
+	jump_force 	= Vector2(0, -300)
+	
+	can_jump 	= true
+	is_jump 	= false
+	in_air 		= false
 
 func set_funcs_refs(names):
 	for n in names:
@@ -43,9 +52,9 @@ func _physics_process(delta):
 	else: velocity.x = 0
 	if funcs_masks[state][Func.VMOVE]: vmove()
 	else: velocity.y = 0
-		
-	funcs_refs[state].call_func(delta)
+	
 	move()
+	funcs_refs[state].call_func(delta)
 
 func input():
 	direction.x = 0
@@ -54,8 +63,9 @@ func input():
 	if Input.is_action_pressed("ui_left"):
 		direction.x -= 1
 	if Input.is_action_pressed("ui_accept"):
-		pass
-		
+		if can_jump:
+			is_jump = true
+			
 func hmove():
 	velocity.x = direction.x * speed.x
 	
@@ -66,16 +76,36 @@ func move():
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 func idle_state(delta):
-	pass
+	if velocity.x:
+		state = State.RUNNING
+	
+	if is_jump:
+		state = State.JUMPING
 	
 func running_state(delta): 
-	pass
+	if !velocity.x && !velocity.y:
+		state = State.IDLE	
 	
+	if is_jump:
+		state = State.JUMPING
+		
 func jumping_state(delta):
-	pass
+	if can_jump:
+		can_jump = false
+		velocity.y = jump_force.y
 	
+	if velocity.y > 0:
+		state = State.FALLING
+		
 func falling_state(delta):
-	pass
+	if is_on_floor():
+		state = State.LANDING
+		can_jump = true
+		is_jump = false
 	
 func landing_state(delta):
-	pass
+	if !velocity.x:
+		state = State.IDLE
+	
+	if velocity.x:
+		state = State.RUNNING
